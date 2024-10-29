87e244426752fcd7fced5fa8d26d8fb29e66ad61
angelalves <angeltrashangel@gmail.com>
2024-10-29 09:58:46 -0300

    feat: ensure AuthUseCase have the same email as httRequest

--- src/presentation/routers/login-router.spec.ts
+++ src/presentation/routers/login-router.spec.ts
@@ -2,12 +2,22 @@ import { HttpRequest } from "../protocols/http"
 import { LoginRouter } from "./login-router"
 
 const makeSut = () => {
-  return new LoginRouter()
+  class AuthUseCase {
+    email!: string
+    auth(email: string) {
+      this.email = email
+    }
+  }
+
+  const authUseCase = new AuthUseCase()
+
+  const sut = new LoginRouter(authUseCase)
+  return { sut, authUseCase }
 }
 
 describe("Login Router", () => {
   it("Should return 400 if no email is provided", () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest: HttpRequest = {
       body: {
@@ -20,7 +30,7 @@ describe("Login Router", () => {
   })
 
   it("Should return 400 if no password is provided", () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest: HttpRequest = {
       body: {
@@ -33,7 +43,7 @@ describe("Login Router", () => {
   })
 
   it("Should return 500 if no body is provided", () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest: HttpRequest = {}
     const httpResponse = sut.route(httpRequest)
@@ -41,10 +51,23 @@ describe("Login Router", () => {
   })
 
   it("Should return 500 if no httpRequest is provided", () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpResponse = sut.route()
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toBe("internal server error")
+  })
+
+  it("Should call AuthUseCase with correct params ", () => {
+    const { sut, authUseCase } = makeSut()
+    const httpRequest: HttpRequest = {
+      body: {
+        email: "angie@gmail.com",
+        password: "123",
+      },
+    }
+    sut.route(httpRequest)
+
+    expect(authUseCase.email).toBe(httpRequest.body?.email)
   })
 })
--- src/presentation/routers/login-router.ts
+++ src/presentation/routers/login-router.ts
@@ -1,7 +1,13 @@
 import { serverError, badRequest } from "../helpers/http.helper"
 import { HttpRequest, HttpResponse } from "../protocols/http"
 
+interface AuthUseCase {
+  auth(email: string): void
+}
+
 export class LoginRouter {
+  constructor(private readonly authUseCase: AuthUseCase) {}
+
   route(httpRequest?: HttpRequest): HttpResponse {
     if (!httpRequest || !httpRequest.body)
       return serverError("internal server error")
@@ -9,6 +15,8 @@ export class LoginRouter {
     const { email, password } = httpRequest!.body!
     if (!email) return badRequest("email is missing")
     if (!password) return badRequest("password is missing")
+
+    this.authUseCase.auth(email)
 
     return { statusCode: 200 }
   }
