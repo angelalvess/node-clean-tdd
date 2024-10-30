50fd7918b026a8c96f983943f3cf04a31c3c4136
angelalves <angeltrashangel@gmail.com>
2024-10-30 13:16:58 -0300

    feat: ensure AuthUseCase calls loadUserByEmailRepository if the right email

--- src/domain/auth-usecase.spec.ts
+++ src/domain/auth-usecase.spec.ts
@@ -1,7 +1,13 @@
 import { IAuthUseCase } from "@/presentation/routers/login-router"
 import { MissingParamError } from "@/utils/errors"
 
+interface ILoadUserByEmailRepository {
+  load(email: string): Promise<void>
+}
 class AuthUseCase implements IAuthUseCase {
+  constructor(
+    private readonly loadUserByEmailRepository?: ILoadUserByEmailRepository,
+  ) {}
   async auth(email?: string, password?: string): Promise<string | null | void> {
     if (!email) {
       throw new MissingParamError("email")
@@ -9,21 +15,42 @@ class AuthUseCase implements IAuthUseCase {
     if (!password) {
       throw new MissingParamError("password")
     }
+
+    await this.loadUserByEmailRepository?.load(email)
   }
+}
+
+const makeSut = () => {
+  return new AuthUseCase()
 }
 
 describe("Auth Usecase", () => {
   it("Should throw if no email is provided", async () => {
-    const sut = new AuthUseCase()
+    const sut = makeSut()
 
     const promise = sut.auth()
     expect(promise).rejects.toThrow(new MissingParamError("email"))
   })
 
   it("Should throw if no password is provided", async () => {
-    const sut = new AuthUseCase()
+    const sut = makeSut()
 
     const promise = sut.auth("any_email@gmail.com")
     expect(promise).rejects.toThrow(new MissingParamError("password"))
+  })
+
+  it("Should call LoadUserByEmailRepository with correct email", async () => {
+    class LoadUserByEmailRepositorySpy {
+      email!: string
+      async load(email: string) {
+        this.email = email
+      }
+    }
+
+    const loadUserByEmailRepository = new LoadUserByEmailRepositorySpy()
+    const sut = new AuthUseCase(loadUserByEmailRepository)
+
+    await sut.auth("any_email@gmail.com", "any_password")
+    expect(loadUserByEmailRepository.email).toBe("any_email@gmail.com")
   })
 })
