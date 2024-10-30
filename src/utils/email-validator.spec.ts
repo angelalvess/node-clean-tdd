b8c552ba036bddf8b888d98bba6b91c407a9f673
angelalves <angeltrashangel@gmail.com>
2024-10-30 09:49:30 -0300

    feat: ensure EmailValidator return false if isEmail is invalid

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -1,9 +1,23 @@
-import { EmailValidator } from "./email-validator"
+import validator from "validator"
+
+class EmailValidator {
+  isValid(email: string): boolean {
+    return validator.isEmail(email)
+  }
+}
 
 describe("Email Validator", () => {
-  test("Should return true if validator returns true", () => {
+  it("Should return true if validator returns true", () => {
     const sut = new EmailValidator()
+
     const isEmailValid = sut.isValid("angelalves@gmail.com")
     expect(isEmailValid).toBe(true)
+  })
+
+  it("Should return false if validator returns false", () => {
+    const sut = new EmailValidator()
+
+    const isEmailValid = sut.isValid("invalid_email")
+    expect(isEmailValid).toBe(false)
   })
 })
