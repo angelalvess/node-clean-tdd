eda4c616d8451bebf15a8d43f113e844d756589c
angelalves <angeltrashangel@gmail.com>
2024-10-30 09:51:15 -0300

    feat: move sut creation to a factory helper method

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -6,16 +6,20 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe("Email Validator", () => {
   it("Should return true if validator returns true", () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
 
     const isEmailValid = sut.isValid("angelalves@gmail.com")
     expect(isEmailValid).toBe(true)
   })
 
   it("Should return false if validator returns false", () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
 
     const isEmailValid = sut.isValid("invalid_email")
     expect(isEmailValid).toBe(false)
--- src/utils/email-validator.ts
+++
@@ -1,6 +0,0 @@
-export class EmailValidator {
-  isValid(email: string): boolean {
-    console.log(`The email is ${email}`)
-    return true
-  }
-}
