bbe323ca40d2f634b727b2494f9280384d4ffe0c
angelalves <angeltrashangel@gmail.com>
2024-11-01 12:28:34 -0300

    feat: move Encrypter to a sut factory function

--- src/utils/helpers/encrypter.spec.ts
+++ src/utils/helpers/encrypter.spec.ts
@@ -8,16 +8,21 @@ class Encrypter {
   }
 }
 
+const makeSut = () => {
+  const sut = new Encrypter()
+  return { sut }
+}
+
 describe("Encrypter", () => {
   it("Should return true if bcrypt return true", async () => {
-    const sut = new Encrypter()
+    const { sut } = makeSut()
     const isValid = await sut.compare("any_value", "hashed_value")
 
     expect(isValid).toBe(true)
   })
 
   it("Should return false if bcrypt return false", async () => {
-    const sut = new Encrypter()
+    const { sut } = makeSut()
     bcrypt.isValid = false
     const isValid = await sut.compare("any_value", "hashed_value")
 
@@ -25,7 +30,7 @@ describe("Encrypter", () => {
   })
 
   it("Should call bcript if the right values", async () => {
-    const sut = new Encrypter()
+    const { sut } = makeSut()
     await sut.compare("any_value", "hashed_value")
 
     expect(bcrypt.value).toBe("any_value")
