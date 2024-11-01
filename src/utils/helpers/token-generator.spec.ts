3e6de0032a90686af9dfd9c9d6a98a65c68d6827
angelalves <angeltrashangel@gmail.com>
2024-11-01 13:32:30 -0300

    refactor: move TokenGenerator to a sut factory function helper

--- src/utils/helpers/token-generator.spec.ts
+++ src/utils/helpers/token-generator.spec.ts
@@ -11,21 +11,26 @@ class TokenGenerator implements ITokenGenerator {
   }
 }
 
+const makeSut = () => {
+  const sut = new TokenGenerator()
+  return { sut }
+}
+
 describe("Token Generator", () => {
   it("Should return null if JWT returns null", async () => {
-    const sut = new TokenGenerator()
+    const { sut } = makeSut()
     jwt.token = null
     const token = await sut.generate("any_id")
     expect(token).toBeNull()
   })
 
   it("Should throw if no params is provided", async () => {
-    const sut = new TokenGenerator()
+    const { sut } = makeSut()
     expect(sut.generate()).rejects.toThrow(new MissingParamError("id"))
   })
 
   it("Should return a token if JWT returns a token", async () => {
-    const sut = new TokenGenerator()
+    const { sut } = makeSut()
     const token = await sut.generate("any_id")
     expect(token).toBe(jwt.token)
   })
