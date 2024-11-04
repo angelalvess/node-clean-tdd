27606bfc86766816df5571a045dd56e1d1a54c33
angelalves <angeltrashangel@gmail.com>
2024-11-04 12:10:32 -0300

    feat: ensure UpdateAccessTokenRepository throws if no userModel is provid

--- src/infra/repositories/update-access-token-repository.spec.ts
+++ src/infra/repositories/update-access-token-repository.spec.ts
@@ -58,4 +58,15 @@ describe("UpdateAccessToken Repository", () => {
     })
     expect(updatedFakeUser!.accessToken).toBe("valid_token")
   })
+  it("Should throw if no userModel is provided", async () => {
+    const { userModel } = makeSut()
+    const sut = new UpdateAccessTokenRepository()
+    const fakeUser = await userModel.insertOne({
+      email: "valid_email@gmail.com",
+      password: "hashed_password",
+    })
+    const promise = sut.update(fakeUser.insertedId, "valid_token")
+
+    expect(promise).rejects.toThrow()
+  })
 })
