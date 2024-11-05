53ebf7f0962b65a527c87be757c279a0db1569fa
angelalves <angeltrashangel@gmail.com>
2024-11-05 13:07:21 -0300

    feat: integrate dotenv for environment variable management and update server initialization

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import express from "express"
+import dotenv from "dotenv"
 import cors from "cors"
+import { MongoHelper } from "./infra/helpers/mongo-helper"
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const main = async () => {
+  dotenv.config()
+  const PORT = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-  res.send("Hello World mango")
-})
+  const URL =
+    process.env.MONGO_URL || "mongodb://localhost:27017/todo-crud-node"
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000")
-})
+  await MongoHelper.connect(URL)
+
+  const app = express()
+  app.use(cors())
+  app.use(express.json())
+
+  app.get("/", (req, res) => {
+    res.send("Hello World mango")
+  })
+
+  app.listen(PORT, () => {
+    console.log("Server is running on port 3000")
+  })
+}
+
+main()
